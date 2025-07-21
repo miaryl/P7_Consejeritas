5320733cd05bf587dba03b6f2c694c3e3a4a89e0
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-07-21 12:45:04 +0200

    created favourite.js

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
-const btn = document.getElementById("fetchBtn"); 
-const showFact = document.getElementById("fact");
-
-const originalBtnText = btn.textContent;
-
-btn.addEventListener('click', fetchFact);
-
-function fetchFact() {
-    btn.textContent = "Buscando consejito...";
-    btn.disabled = true; 
-
-    fetch('https://uselessfacts.jsph.pl/random.json?language=en')
-    .then(response => response.json())
-    .then(data => {
-        showFact.textContent = data.text;
-        
-        btn.textContent = originalBtnText;
-        btn.disabled = false;
-    })
-    .catch(error => {
-        showFact.textContent = "Lo sentimos, ha ocurrido un error.";
-        console.error('Error:', error);
-        
-        btn.textContent = originalBtnText;
-        btn.disabled = false;
-    })
-}
+(() => {
+    const btn = document.getElementById("fetchBtn"); 
+    const showFact = document.getElementById("fact");
+  
+    btn.addEventListener('click', fetchFact);
+  
+    function fetchFact(){
+        fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+        .then(response => response.json())
+        .then(data => {
+            showFact.textContent = data.text;
+        })
+        .catch(error => {
+            showFact.textContent = "sorry there is some error...";
+            console.error('error', error);
+        });
+    }
+  })();
